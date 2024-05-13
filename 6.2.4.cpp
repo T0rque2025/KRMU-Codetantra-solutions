bf7a7eb400a4761f6faafb9ed802e8cd13ba62f5
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:13:59 +0530

    Create 6.2.4.cpp

---
+++ 6.2.4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void ap_capital(void);
+void ts_capital(void);
+int main() {
+	 ap_capital();
+	ts_capital();
+}
+void ap_capital(void) {
+	cout << "Amaravathi\n";
+}
+void ts_capital(void) {
+	cout << "Hyderabad\n";
+}
