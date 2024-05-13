5746278c8d57875e3606988d8c15ca9340240350
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:33:41 +0530

    Create 8.3.3.cpp

---
+++ 8.3.3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char number = '9';
+	cout << "Given character = " <<number  << "\n";
+	cout << "Given character's ASCII value = " <<static_cast<int>(number)  << "\n";
+}
