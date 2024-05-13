b6b91ee8964ab78ce64ac5b5478ccd1ffe3a5835
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:26:29 +0530

    Create 7.3.5.cpp

---
+++ 7.3.5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int number1 = 20;
+	int number2 = 30;
+	int sub = number1 - number2;
+	cout << "The difference of the two given numbers = " << sub << "\n";
+}
