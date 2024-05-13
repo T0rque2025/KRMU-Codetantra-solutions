0e619dc9420ac29402a702b85bf1a827a170e54b
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:28:14 +0530

    Create 7.4.2.cpp

---
+++ 7.4.2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	    short int num1, num2;
+	    cout << "Enter two short integers : ";
+	    cin >> num1 >> num2;
+	    int product = num1 * num2;
+	    cout << "Product = " << product << endl;
+}
