11fc40f3ba53d0a662ade20c03b7e5ede916b1d3
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:29:28 +0530

    Create 7.5.2.cpp

---
+++ 7.5.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+	    long int num1, num2;
+
+	    cout << "Enter two long ints : ";
+
+	    cin >> num1 >> num2;
+
+	    long int result = num1 - num2;
+
+	    cout << "Result = " << result << "\n";
+	    return 0;
+
+}
