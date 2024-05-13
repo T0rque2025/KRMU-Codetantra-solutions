8c7264fbf0f06dfb8442c4d13c7a2360e15b359a
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:24:53 +0530

    Create 7.3.2.cpp

---
+++ 7.3.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int decimalOne = 1;
+	cout << "decimalOne value = " << decimalOne << "\n";
+	int binaryThree = 0b11;
+	cout << "binaryThree value = " << binaryThree << "\n";
+	int octalEight = 010;
+	cout << "octalEight value = " << octalEight << "\n";
+	int hexTen = 0xA;
+	cout << "hexTen value = "<< hexTen << "\n";
+	int asciiValueOfOne = '1';
+	cout << "asciiValueOfOne value = " << asciiValueOfOne << "\n";
+	int asciiValueOfA = 'A';
+	cout << "asciiValueOfA value = " <<asciiValueOfA << "\n";
+	int asciiValueOfZ = 'Z';
+	cout << "asciiValueOfZ value = " << asciiValueOfZ << "\n";
+}
