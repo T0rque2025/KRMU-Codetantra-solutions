1803aebda899729cee928c94486dab60b920ae0a
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:35:30 +0530

    Create 8.5.1.cpp

---
+++ 8.5.1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+	wchar_t w = 'AB'; // Write the correct data type
+	cout << "Wide character value = " << w << '\n';
+	cout << "Size of the wide char = " << sizeof(w) << "\n";
+}
