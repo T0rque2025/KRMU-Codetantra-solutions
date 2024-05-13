68c574e96841023a477023dc4248941f2abcd66c
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:46:38 +0530

    Create 9.2.1.cpp

---
+++ 9.2.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num1, num2;
+	cout << "Enter two integer values : ";
+	cin >> num1 >> num2;
+	cout << "Addition Result = " <<(num1+num2)  << endl;
+	cout << "Subtraction Result = " <<(num1-num2)  << endl;
+	cout << "Multiplication Result = " <<(num1*num2)  << endl;
+	cout << "Division Result = " << (num1/num2) << endl;
+	cout << "Remainder = " <<(num1%num2)  << endl;
+}
