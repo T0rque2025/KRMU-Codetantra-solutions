08dc0aa0bfed2de2e05b02a29141044cf026c028
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:25:38 +0530

    Create 7.3.3.cpp

---
+++ 7.3.3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+	    int n1, n2;
+	    cout << "Enter n1 n2 : ";
+	    cin >> n1 >> n2;
+	    int sum = n1 + n2;
+	    cout << "Sum of " << n1 << " and " << n2 << " = " << sum << endl;
+	    return 0;
+
+}
