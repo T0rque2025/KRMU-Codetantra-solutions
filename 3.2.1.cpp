07e39866a04bf67a476972bdba98721a32176a3b
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 13:58:12 +0530

    Create 3.2.1.cpp

---
+++ 3.2.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	cout<<"I am learning C++ programming!\n";
+	int a=10;
+	int b=20;
+	int sum;
+	sum = a+b;
+	
+	cout<<"Sum of two integer numbers = "<<sum<<endl;
+}
