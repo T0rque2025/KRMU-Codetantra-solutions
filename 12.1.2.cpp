7726a724c048e92404ea38cf58bad23e46e406a4
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 15:44:51 +0530

    Create 12.1.2.cpp

---
+++ 12.1.2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	cout << "Enter a number : ";
+	cin>>num;
+	if (num%5==0 ) { // Write your condition here
+		cout << "Given number " << num << " is divisible by 5\n"; // complete the code here
+	}
+}
