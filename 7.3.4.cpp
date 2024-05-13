a9854737138c719d3da41f0b133dd74b8312fd1a
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:26:06 +0530

    Create 7.3.4.cpp

---
+++ 7.3.4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	signed int number1 = -20, number2 = 20;
+	unsigned int number3 = -1, number4 = 1;
+	cout << "Given signed values are " << number1 << " and " <<number2 << "\n"; // Fill in the missing code to print number1, number2
+	cout << "Given unsigned values are " << number3 << " and " << number4 << "\n"; // Fill in the missing code to print number3, number4
+}
