8d9a0faefb7dc590676710a3ac2b7fcbf4eab509
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-13 14:47:13 +0530

    Create 9.2.2.cpp

---
+++ 9.2.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float num1, num2;
+	cout << "Enter two float values : ";
+	cin>>num1>>num2;
+	cout << "Result of addition = " <<num1+num2  << endl;
+	cout << "Result of subtraction = " << num1-num2 << endl;
+	cout << "Result of multiplication = " << num1*num2 << endl;
+	cout << "Result of division = " <<num1/num2  << endl;
+}
