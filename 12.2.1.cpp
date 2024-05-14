785d61c8cf7b6694c46a3e2deeefd3bf95050c55
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-14 14:33:45 +0530

    Create 12.2.1.cpp

---
+++ 12.2.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	    int marks;
+	    cout << "Enter marks : ";
+
+	    cin >> marks;
+	    if (marks > 75) {
+			        cout << "Distinction." << endl;
+		} else {
+			        cout << "Not distinction." << endl;
+
+		}
+	    return 0;
+
+}
