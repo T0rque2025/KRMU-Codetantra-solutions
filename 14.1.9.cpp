c98f864cfeca40dcebdc82c37bd6fe6a63f9f48c
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-16 19:49:16 +0530

    Create 14.1.9.cpp

---
+++ 14.1.9.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+int main() {
+
+	    int age1, age2;
+
+	    cout << "Enter two integer values : ";
+
+	    cin >> age1 >> age2;
+
+
+
+	    if (age1 < 10 && age2 > 60) {
+
+			        cout << "Discount Price\n";
+
+		} else {
+
+			        cout << "Full Price\n";
+
+		}
+
+
+
+	    return 0;
+
+}
