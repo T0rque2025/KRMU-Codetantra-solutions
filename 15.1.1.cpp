8032be429227e5e560d0c59f0964746ef90ed2fd
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-16 19:53:33 +0530

    Create 15.1.1.cpp

---
+++ 15.1.1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+int main() {
+
+	    int num, sum = 0;
+
+
+
+	    while (sum <= 100) {
+
+			        cout << "Enter a number : ";
+
+			        cin >> num;
+
+			        sum += num;
+
+		}
+
+
+
+	    cout << "The total of given numbers is : " << sum << endl;
+
+
+
+	    return 0;
+
+}
