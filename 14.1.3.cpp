a64450b64f0189a5e44007e1df9616b7cea4164e
Gunjan Joshi <132914267+T0rque2025@users.noreply.github.com>
2024-05-16 19:45:40 +0530

    Create 14.1.3.cpp

---
+++ 14.1.3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+int main() {
+
+	    int hour;
+
+	    cout << "Enter hour : ";
+
+	    cin >> hour;
+
+
+
+	    if (hour >= 9 && hour <= 17) {
+
+			        cout << "Working hour\n";
+
+		} else {
+
+			        cout << "Non-working hour\n";
+
+		}
+
+
+
+	    return 0;
+
+}
